f59dae830bc39a3b4bfd5b40184b7d987d6c0258
Yong Cheng <will.chengyong@gmail.com>
2019-04-23 18:20:54 +0800

    added external middleware logger

--- src/index.js
+++ src/index.js
@@ -4,25 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore, applyMiddleware} from 'redux';
+// 引入外部中間件
+import logger from 'redux-logger';
+
 import rootReducer from './reducers';
 import { increment, decrement } from './actions/action';
 import { Provider } from "react-redux";
 
-const logger = store => next => action => {
-    console.log('dispatching', action);
-    let result = next(action); //調用下一個中間件（error）
-    console.log('next state', store.getState());
-    return result;
-};
+// const logger = store => next => action => {
+//     console.log('dispatching', action);
+//     let result = next(action); //調用下一個中間件（error）
+//     console.log('next state', store.getState());
+//     return result;
+// };
 
-
-const error = store => next => action => {
-    try{
-        next(action);
-    }catch(e){
-        console.log(e);
-    }
-}
+// const error = store => next => action => {
+//     try{
+//         next(action); //到 reducer
+//     }catch(e){
+//         console.log(e);
+//     }
+// }
 
 //白話文
 const logger_exp = function(store){
@@ -37,7 +39,7 @@ const logger_exp = function(store){
 }
 
 //中間件日誌紀錄
-const store = createStore(rootReducer, {}, applyMiddleware(logger, error)); //依序執行
+const store = createStore(rootReducer, {}, applyMiddleware(logger)); //依序執行
 
 
 
